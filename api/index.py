e7d497987a7be2bb86aae7237ac41b504219aa53
Aidan Gibson <tronicdude@gmail.com>
2023-12-28 00:06:14 -0800

    accepts any url

--- api/index.py
+++ api/index.py
@@ -12,7 +12,6 @@ def track(path):
     requester_ip = request.remote_addr
     access_time = datetime.now()
 
-    print('Path: %s' % path)
+    print(f"Path: {path} IP: {requester_ip} Time: {access_time}")
     # Log the IP and time TODO later send this to database. sqlite would be fine but i want practice w influxdb. this could be separate server or use influxdb http api for sep server. have failsafe if the server is down.
-    print(f"IP: {requester_ip} Time: {access_time}") #TODO also include the url they requested with whatever other deets
     return send_file("boo.png", mimetype="image/png")
