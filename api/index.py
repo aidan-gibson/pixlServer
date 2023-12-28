1d97aae0f2a8a74a10b773746edddacd88560ef7
Aidan Gibson <tronicdude@gmail.com>
2023-12-28 00:02:42 -0800

    url change

--- api/index.py
+++ api/index.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 
-def track():
+def track(path):
     # Capture the requester's IP and the current time
     requester_ip = request.remote_addr
     access_time = datetime.now()
 
+    print('You want path: %s' % path)
     # Log the IP and time TODO later send this to database. sqlite would be fine but i want practice w influxdb. this could be separate server or use influxdb http api for sep server. have failsafe if the server is down.
     print(f"IP: {requester_ip} Time: {access_time}") #TODO also include the url they requested with whatever other deets
     return send_file("boo.png", mimetype="image/png")
